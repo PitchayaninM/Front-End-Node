880de2fac64c098c009eea62e0486545a8eb3148
PitchayaninM <pitchayaninmakoon@gmail.com>
2023-09-19 11:29:52 +0700

    update

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API 
 //const base_url = "https://api.example.com"; 
-const base_url = "http://localhost:5500"; 
+const base_url = "http://node50266-pitchayanin.proen.app.ruk-com.cloud:11494"; 
 
 // Set the template engine 
 app.set('view engine', 'ejs'); 
