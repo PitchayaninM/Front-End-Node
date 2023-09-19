00c6e560af4f305734274e7a076593b4048e9224
PitchayaninM <pitchayaninmakoon@gmail.com>
2023-09-19 19:01:50 +0700

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API 
 //const base_url = "https://api.example.com"; 
-const base_url = "http://50274"; 
+const base_url = "http://10.104.9.15"; 
 
 
 // Set the template engine 
