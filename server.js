003437e67f1b27c34dbde6734bb8462cad37f1da
PitchayaninM <pitchayaninmakoon@gmail.com>
2023-09-26 09:34:40 +0700

    update

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API 
 //const base_url = "https://api.example.com"; 
-//const base_url = "http://10.104.9.15"; 
-const base_url = "http://localhost:3000";
+const base_url = "http://10.104.9.15"; 
+//const base_url = "http://localhost:3000";
 
 
 // Set the template engine 
@@ -98,6 +98,6 @@ app.get("/delete/:id", async (req, res) => {
 }); 
 
 
-app.listen(5500, () => { 
-            console.log('Server started on port 5500'); 
+app.listen(8080, () => { 
+            console.log('Server started on port 8080'); 
             });
